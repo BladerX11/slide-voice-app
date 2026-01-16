362d9bb2847ed4581169c935558e05ff966fd73d
BladerX11 <BladerX11@proton.me>
2026-01-17 00:01:57 +0800

    chore: switch to package structure

--- src/slide_voice_app/__main__.py
+++ src/slide_voice_app/__main__.py
@@ -6,13 +6,19 @@ from PySide6.QtCore import QUrl
 from PySide6.QtQml import QQmlApplicationEngine
 
 
-if __name__ == "__main__":
+def main():
     app = QGuiApplication()
     engine = QQmlApplicationEngine()
-    qml_file = Path(__file__).parent / "main.qml"
+    qml_file = Path(__file__).parent / "ui" / "Main.qml"
     engine.load(QUrl.fromLocalFile(qml_file))
+
     if not engine.rootObjects():
         sys.exit(-1)
+
     exit_code = app.exec()
     del engine
     sys.exit(exit_code)
+
+
+if __name__ == "__main__":
+    main()
