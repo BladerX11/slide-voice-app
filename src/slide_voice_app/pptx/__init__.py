bd49f445902c90bfe93947f2da5f9557c4c7f886
BladerX11 <BladerX11@proton.me>
2026-02-03 16:54:09 +0800

    feat: add functions to insert audio

--- src/slide_voice_app/pptx/__init__.py
+++ src/slide_voice_app/pptx/__init__.py
@@ -1,7 +1,9 @@
 """PPTX manipulation module for reading and writing PowerPoint files."""
 
+from .audio_insert import save_pptx_with_audio
 from .package import PptxPackage
 
 __all__ = [
     "PptxPackage",
+    "save_pptx_with_audio",
 ]
